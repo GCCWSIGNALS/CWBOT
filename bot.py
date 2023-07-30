5300c02b147213dba29ddfdbf723706a8089e204
GCCWSIGNALS <140855338+GCCWSIGNALS@users.noreply.github.com>
2023-07-30 23:43:16 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,79 +3,88 @@ from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext, CallbackQueryHandler
 
 # Telegram API token
-TELEGRAM_API_TOKEN = "6069137445:AAGoUQm1UVt0sfGpCqKcMxLgLwCTZhPBH9c"  # Replace with your actual token
+TELEGRAM_API_TOKEN = "6069137445:AAGoUQm1UVt0sfGpCqKcMxLgLwCTZhPBH9c"
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 def start(update: Update, _: CallbackContext):
-    keyboard = [
-        [InlineKeyboardButton("Price Plan", callback_data='price_plan')],
-        [InlineKeyboardButton("Our Services", callback_data='services')],
-        [InlineKeyboardButton("Our Signals", callback_data='signals')]
-    ]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    update.message.reply_text(
-        "Welcome to Our Friendly Bot! 😊\n\n"
-        "I'm here to assist you with our services and signals.\n\n"
-        "Please choose an option below:",
-        reply_markup=reply_markup
-    )
+    try:
+        keyboard = [
+            [InlineKeyboardButton("Price Plan", callback_data='price_plan')],
+            [InlineKeyboardButton("Our Services", callback_data='services')],
+            [InlineKeyboardButton("Our Signals", callback_data='signals')]
+        ]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        update.message.reply_text(
+            "Welcome to Our Friendly Bot! 😊\n\n"
+            "I'm here to assist you with our services and signals.\n\n"
+            "Please choose an option below:",
+            reply_markup=reply_markup
+        )
+    except Exception as e:
+        logging.error("An error occurred during start handler: %s", str(e))
 
 def button_click(update: Update, _: CallbackContext):
-    query = update.callback_query
-    option = query.data
+    try:
+        query = update.callback_query
+        option = query.data
 
-    if option == 'price_plan':
-        query.message.reply_text(
-            "Our Price Plan:\n\n"
-            "🌟 7-day free trial\n"
-            "🌟 After the free trial, the subscription is £34.99/month.\n\n"
-            "Subscribe now to get started!"
-        )
-    elif option == 'services':
-        query.message.reply_text(
-            "Our Services:\n\n"
-            "🔹 Live Trading Sessions\n"
-            "🔹 VIP Forex & Gold Signals\n"
-            "🔹 Supportive and Helpful Active Community\n\n"
-            "Join us now and enjoy our premium services!"
-        )
-    elif option == 'signals':
-        query.message.reply_text(
-            "Our Signals:\n\n"
-            "🔹 Buy/Sell Signals with Entry and Exit Prices\n"
-            "🔹 Take Profit (TP) and Stop Loss (SL) Levels\n"
-            "🔹 Copy the signals and follow our expert analysis\n\n"
-            "Start making profitable trades with our signals!"
-        )
+        if option == 'price_plan':
+            query.message.reply_text(
+                "Our Price Plan:\n\n"
+                "🌟 7-day free trial\n"
+                "🌟 After the free trial, the subscription is £34.99/month.\n\n"
+                "Subscribe now to get started!"
+            )
+        elif option == 'services':
+            query.message.reply_text(
+                "Our Services:\n\n"
+                "🔹 Live Trading Sessions\n"
+                "🔹 VIP Forex & Gold Signals\n"
+                "🔹 Supportive and Helpful Active Community\n\n"
+                "Join us now and enjoy our premium services!"
+            )
+        elif option == 'signals':
+            query.message.reply_text(
+                "Our Signals:\n\n"
+                "🔹 Buy/Sell Signals with Entry and Exit Prices\n"
+                "🔹 Take Profit (TP) and Stop Loss (SL) Levels\n"
+                "🔹 Copy the signals and follow our expert analysis\n\n"
+                "Start making profitable trades with our signals!"
+            )
+    except Exception as e:
+        logging.error("An error occurred during button_click handler: %s", str(e))
 
 def unknown(update: Update, _: CallbackContext):
-    update.message.reply_text(
-        "I'm sorry, I don't understand that command. Please choose one of the options below:",
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Price Plan", callback_data='price_plan')],
-                                          [InlineKeyboardButton("Our Services", callback_data='services')],
-                                          [InlineKeyboardButton("Our Signals", callback_data='signals')]])
-    )
+    try:
+        update.message.reply_text(
+            "I'm sorry, I don't understand that command. Please choose one of the options below:",
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Price Plan", callback_data='price_plan')],
+                                              [InlineKeyboardButton("Our Services", callback_data='services')],
+                                              [InlineKeyboardButton("Our Signals", callback_data='signals')]])
+        )
+    except Exception as e:
+        logging.error("An error occurred during unknown handler: %s", str(e))
 
 def main():
-    # Print the token to the console for debugging purposes
-    print(f"Token: {TELEGRAM_API_TOKEN}")
+    try:
+        # Print the token to the console for debugging purposes
+        print(f"Token: {TELEGRAM_API_TOKEN}")
+        
+        updater = Updater(token=TELEGRAM_API_TOKEN, use_context=True)
+        dispatcher = updater.dispatcher
 
-    if TELEGRAM_API_TOKEN is None:
-        raise ValueError("Telegram API token is not provided. Please set TELEGRAM_API_TOKEN variable.")
-    
-    updater = Updater(token=TELEGRAM_API_TOKEN, use_context=True)
-    dispatcher = updater.dispatcher
+        # Add handlers
+        dispatcher.add_handler(CommandHandler("start", start))
+        dispatcher.add_handler(MessageHandler(Filters.command, unknown))
+        dispatcher.add_handler(CallbackQueryHandler(button_click))
 
-    # Add handlers
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(MessageHandler(Filters.command, unknown))
-    dispatcher.add_handler(CallbackQueryHandler(button_click))
-
-    # Start the bot
-    updater.start_polling()
-    updater.idle()
+        # Start the bot
+        updater.start_polling()
+        updater.idle()
+    except Exception as e:
+        logging.error("An error occurred during bot execution: %s", str(e))
 
 if __name__ == "__main__":
     main()
